9e5b5be21af5fb021e53ce62d19531b5336e4673
Rem Wransky <13735131+RWransky@users.noreply.github.com>
2021-05-13 14:07:12 -0400

    changes to docker-compose and entrypoint handling to work with latest Docker

--- cmd/server/entrypoint.sh
+++ cmd/server/entrypoint.sh
@@ -1,6 +1,4 @@
-#!/bin/bash -e
-
-exec > >(tee -a /var/log/app/entry.log|logger -t server -s 2>/dev/console) 2>&1
+#!/bin/bash
 
 APP_ENV=${APP_ENV:-local}
 
@@ -16,4 +14,4 @@ echo "[`date`] Running DB migrations..."
 migrate -database "${APP_DSN}" -path ./migrations up
 
 echo "[`date`] Starting server..."
-./server -config ${CONFIG_FILE} >> /var/log/app/server.log 2>&1
+./server -config ${CONFIG_FILE}
